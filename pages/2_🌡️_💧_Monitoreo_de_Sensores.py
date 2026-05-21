ba7bce2022ca9b21efcc18e1969149938a6779be
manucitavm-web <manucitavm@gmail.com>
2026-05-21 16:41:15 -0500

    Update 2_🌡️_💧_Monitoreo_de_Sensores.py

--- pages/2_🌡️_💧_Monitoreo_de_Sensores.py
+++ pages/2_🌡️_💧_Monitoreo_de_Sensores.py
@@ -38,7 +38,7 @@ st.markdown("""
 st.markdown("# 🌡️💧 Monitoreo de Sensores")
 
 # Descripción integrada alineada con el estilo de la pestaña anterior
-st.write("Panel de visualización de datos para la cocina inteligente (Smart Kitchen). Esta pestaña de la aplicación ofrece una solución interactiva de monitoreo donde cualquier usuario puede revisar el estado térmico del horno y la humedad del ambiente en tiempo real, facilitando el seguimiento preventivo de las variables climáticas del entorno.")
+st.write("Panel de visualización de datos para tu cocina inteligente (Smart Kitchen). Esta pestaña de la aplicación te ofrece una solución interactiva de monitoreo donde podrás revisar el estado térmico del horno y la humedad del ambiente en tiempo real, facilitando el seguimiento preventivo de las variables climáticas del entorno.")
 st.write("---")
 
 # 📡 Datos de red (Mismo broker de tu trabajo anterior + tu canal único)
