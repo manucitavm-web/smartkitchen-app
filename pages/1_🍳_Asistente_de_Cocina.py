f4f2720be9eeb0f30a9cc5f817e705cace81d339
manucitavm-web <manucitavm@gmail.com>
2026-05-21 14:44:07 -0500

    Update 1_🍳_Asistente_de_Cocina.py

--- pages/1_🍳_Asistente_de_Cocina.py
+++ pages/1_🍳_Asistente_de_Cocina.py
@@ -8,7 +8,7 @@ st.write("Escribe libremente los ingredientes que tienes en tu nevera y nuestra 
 st.write("---")
 
 # 🔐 CLAVE API DE GOOGLE AI STUDIO
-API_KEY = "AIzaSyBGng7EBh0dKFD53KXz7cOapi5e4Pjlp9Q"
+API_KEY = st.text_input("clave")
 
 try:
     genai.configure(api_key=API_KEY)
