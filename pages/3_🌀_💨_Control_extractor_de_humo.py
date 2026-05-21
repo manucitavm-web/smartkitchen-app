8bac377c3637f63b91e339515e59ef9d8b227777
manucitavm-web <manucitavm@gmail.com>
2026-05-21 16:34:02 -0500

    Update 3_🌀_💨_Control_extractor_de_humo.py

--- pages/3_🌀_💨_Control_extractor_de_humo.py
+++ pages/3_🌀_💨_Control_extractor_de_humo.py
@@ -5,7 +5,7 @@ import json
 import platform
 
 # --- CONFIGURACIÓN ESTÉTICA (SÓLO VISUAL) ---
-st.set_page_config(page_title="Control de extractor de humo", page_icon="🌀")
+st.set_page_config(page_title="Control de Extractor de Humo", page_icon="🌀")
 
 st.markdown("""
     <style>
